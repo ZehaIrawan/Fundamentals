fd4a0ba3597fb15e846f585caa4d793ed50c4613
Zeha Irawan <zehairawan@gmail.com>
2025-12-10 06:13:42 +0800

    private fields

--- js/main.js
+++ js/main.js
@@ -14,12 +14,14 @@
 // 4. Must use **private fields** + **prototype methods**.
 
 
-
+// https://chatgpt.com/c/69349de0-7fe0-8324-9de7-a655f2600c81
+// https://web.dev/learn/javascript/classes/class-fields
 
 
 class RateLimiter {
   constructor(maxRequests, perMs){
-    console.log(`maxRequest: ${maxRequests}, perMs: ${perMs}`)
+    this.maxRequests = maxRequests;
+    this.perMs = perMs;
   }
   
   run(cb){
@@ -31,12 +33,6 @@ class RateLimiter {
 const limiter = new RateLimiter(2, 1000) // max 2 calls per second
 limiter.run(() => console.log("task"))
 
-// Classes are a template for creating objects.
-// Classes in JS are built on prototypes but also have some syntax and semantics that are unique to classes.
-
-// The constructor method is a special method for creating and initializing an object created with a class.
-
-// method in class doesn't need function keyword
 
 // why use this inside constructor
 
